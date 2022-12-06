55a40c41b333482597aa0ae4a5b92cb559f46209
martin <martingomez3696@gmail.com>
2022-12-05 23:56:48 -0500

    listado de nombres

---
+++ matriz.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int i;
+    char listanombres[6][40]={"Martin 80-14-83","Reggie 83-124","jose 8214-123","teresa","maria","pedro"};
+
+    for (i = 0; i < 6; i++)
+    {
+        printf("%s\n",listanombres[i]);
+    }
+}
+
+        
+      
