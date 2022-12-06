90ab19eb775c557c83300140fd48b07892727a90
martin <martingomez3696@gmail.com>
2022-12-06 15:41:25 -0500

    se agrega Cedula y nombres por separado

--- matriz.c
+++ matriz.c
@@ -1,13 +1,22 @@
 #include<stdio.h>
 int main(){
     int i;
-    char listanombres[6][40]={"Martin 80-14-83","Reggie 83-124","jose 8214-123","teresa","maria","pedro"};
+    int x;
+    
+    char listanombres[6][40]={"Martin ","Reggie ","jose ","teresa","maria","pedro"};
+    char Cedulas[6][40]={"1","2","3","4","5","6"};
 
     for (i = 0; i < 6; i++)
     {
-        printf("%s\n",listanombres[i]);
+        printf("%s\t|-|-| %s\n",listanombres[i],Cedulas[i]);
     }
+
+
+
+    
 }
+
+
 
         
       
