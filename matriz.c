bfd52dc05786b92384632cc502afefd42b66d281
martin <martingomez3696@gmail.com>
2022-12-07 18:02:20 -0500

    Se agrega lista de autorizados

--- matriz.c
+++ matriz.c
@@ -8,7 +8,22 @@ int main(){
 
     for (i = 0; i < 6; i++)
     {
-        printf("%s\t|-|-| %s\n",listanombres[i],Cedulas[i]);
+        printf("%s\t| %s\n",listanombres[i],Cedulas[i]);
+    }
+    char estudiante[30];
+int contador=0;
+printf("ingrese la cedula del estudiante");
+scanf("%s", estudiante);
+for (i=0;i<6;i++){
+    if(strcmp(Cedulas[i],estudiante)==0){
+        printf("El estudiante %s %s tiene acceso \n ", listanombres[i], Cedulas[i]);
+    }else{
+        contador++;
+    }
+}
+    if (contador==6)
+    {
+        printf("%s no tiene acceso", estudiante);
     }
 
 
