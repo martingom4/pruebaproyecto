b8a0c2d057d2ed78ffb72a5082a679dcd168e0ce
martin <martingomez3696@gmail.com>
2022-12-07 19:20:42 -0500

    se agrega la matriz punto 1

--- matriz.c
+++ matriz.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 int main(){
     int i;
     int x;
@@ -25,10 +26,19 @@ for (i=0;i<6;i++){
     {
         printf("%s no tiene acceso", estudiante);
     }
-
-
-
+//Imprirmir matriz punto 1 
+    int matriz [3][4]= {{1, 2, 3, 4},
+                        {5, 6, 7, 8},
+                        {9, 10, 11, 12}};
     
+    
+    for(i=0; i<3;i++){
+        for(x=0; x<4;x++){
+            printf("%d\t",matriz[i][x]);
+        }
+        printf("\n");
+    }
+
 }
 
 
