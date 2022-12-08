a0ae0505e3aa845e9a4ad16eea4fb311cdfa84df
martin <martingomez3696@gmail.com>
2022-12-07 22:30:17 -0500

    Punto 1 terminado

--- matriz.c
+++ matriz.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 #include<string.h>
+void VerCompu(int numCompu[][4], int estCompu[][4], char Cedulas[][10] );
 int main(){
     int i;
     int x;
-    
+//punto ver lista de personas que pueden entrar al laboratorio 
     char listanombres[6][40]={"Martin ","Reggie ","jose ","teresa","maria","pedro"};
     char Cedulas[6][40]={"1","2","3","4","5","6"};
 
@@ -11,9 +12,10 @@ int main(){
     {
         printf("%s\t| %s\n",listanombres[i],Cedulas[i]);
     }
-    char estudiante[30];
+//Punto ver si estan registrados o no 
+char estudiante[30];
 int contador=0;
-printf("ingrese la cedula del estudiante");
+printf("ingrese la cedula del estudiante \t");
 scanf("%s", estudiante);
 for (i=0;i<6;i++){
     if(strcmp(Cedulas[i],estudiante)==0){
@@ -24,22 +26,40 @@ for (i=0;i<6;i++){
 }
     if (contador==6)
     {
-        printf("%s no tiene acceso", estudiante);
+        printf("%s no tiene acceso\n", estudiante);
     }
 //Imprirmir matriz punto 1 
-    int matriz [3][4]= {{1, 2, 3, 4},
-                        {5, 6, 7, 8},
-                        {9, 10, 11, 12}};
-    
-    
-    for(i=0; i<3;i++){
-        for(x=0; x<4;x++){
-            printf("%d\t",matriz[i][x]);
+    int matriz [3][4]={{1,2,3,4},
+                        {5,6,7,8},
+                        {9,10,11,12}};
+  
+    int estadoCompu[3][4];
+  
+    for(i=0;i<3;i++){
+        for ( x = 0; x < 4; x++)
+        {
+            estadoCompu[i][x]=-1;
         }
-        printf("\n");
+        
     }
 
-}
+    for (i=0;i<3;i++){
+        for (x= 0; x<4; x++)
+        {
+            if (estadoCompu[i][x]<0)
+            {
+                 printf("%d\t",matriz[i][x]);
+            }
+            else{
+                printf("%s",Cedulas[matriz[i][x]]);
+            }
+           
+        }
+        printf("\n\n");
+        
+    }
+    }
+
 
 
 
